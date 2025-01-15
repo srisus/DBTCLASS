6de8e1e249ac0b0d23280e2aa08b94475bca7a59
Srinivas S <srisus23@gmail.com>
2025-01-15 12:44:05 +0000

    commit_1_15_06_14_PM

--- models/jaffle_shop/staging/stg_orders.sql
+++ models/jaffle_shop/staging/stg_orders.sql
@@ -9,3 +9,15 @@ from  {{source('jaffle_shop','orders')}} --raw.jaffle_shop.orders
 )
 
 select * from orders
+
+--- do we have any new data or not. We can verify source freshness. 
+/*
+Source freshness should be configured in the YML files. 
+To know whethere RAW data has new data or not. When there is new data only , our model will run. 
+We can call it as MD5. 
+We have to do some look ups. 
+
+we refrence a column for source freshness.  Timestamp will be embedded in the table. That column will be used to see the source freshness.
+
+
+*/
