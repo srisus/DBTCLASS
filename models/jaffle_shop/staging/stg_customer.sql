0218f454dbae1e7a2e01b9aa4ae7ad2d3da67a4e
Srinivas S <srisus23@gmail.com>
2025-01-15 12:21:14 +0000

    commit_1_15_2025_05_51_PM

--- models/jaffle_shop/staging/stg_customer.sql
+++ models/jaffle_shop/staging/stg_customer.sql
@@ -1,8 +1,11 @@
 
 with customers as (
 
-select ID as customer_id , first_name , last_name from 
-raw.jaffle_shop.customers
+select 
+ID as customer_id 
+,first_name 
+,last_name 
+from  {{source('jaffle_shop','customers')}} --raw.jaffle_shop.customers
 )
 
 select * from customers
