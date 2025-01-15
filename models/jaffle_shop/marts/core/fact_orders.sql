b518ba7aef8305fa5a87a864441f21dc37c33588
Srinivas S <srisus23@gmail.com>
2025-01-15 10:36:34 +0000

    changes

---
+++ models/jaffle_shop/marts/core/fact_orders.sql
@@ -0,0 +1,22 @@
+with orders as (
+
+    select * from {{ref('stg_orders')}}
+),
+payments as (
+
+    select * from {{ref('stg_payments')}}
+),
+order_payments as (
+
+    select order_id, sum(case when payment_status = 'success' then payment_amount end ) as amount 
+    from payments
+    group by 1
+)
+, final as (
+
+    select orders.order_id, orders.customer_id,orders.order_date , amount 
+    from orders 
+    left join order_payments using(order_id)
+)
+
+select * from final 
